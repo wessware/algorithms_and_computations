b434d47a316c51c1efdcb4a5382695975e0ae9f7
wessware <stevenshisia@yahoo.com>
2024-05-15 21:03:23 +0300

    An additional function that sort strings

--- simple_sort.py
+++ simple_sort.py
@@ -12,3 +12,20 @@ if __name__ == "__main__":
     list_to_sort = [5, 2, 9, 4, 1, 6, 10, 8, 3, 7]
     new_list = sorter(list_to_sort)
     print(new_list)
+
+
+def sort_string(list_of_strings, reverse=False):
+
+
+    if reverse:
+        list_of_strings.sort(reverse=True)
+    else:
+        list_of_strings.sort()
+
+    return list_of_strings
+
+strings = ["wechale", "Rahab", "Esther", "Stephen", "Injendi", "Zakayo", "Mwalati"]
+
+sorted_strings = sort_string(strings)
+
+print(sorted_strings)
